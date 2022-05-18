9a5a91bcb6ffe25516f7129f083ed5280cc16667
roopaish <54392112+Roopaish@users.noreply.github.com>
2022-05-18 08:53:01 +0545

    Custom styling setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { withPlugins } = require("next-compose-plugins");
+const withAntdLess = require("next-plugin-antd-less");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withPlugins(
+  [
+    [
+      withAntdLess,
+      {
+        modifyVars: {
+          "@primary-color": "#1890FF",
+        },
+        cssLoaderOptions: {},
+      },
+    ],
+  ],
+  {
+    reactStrictMode: true,
+  }
+);
+
+module.exports = nextConfig;
---
+++ src/components/style/button/icon.style.tsx
@@ -0,0 +1,7 @@
+import { Button } from "antd";
+import styled from "styled-components";
+
+const IconButton = styled(Button)`
+`
+
+export default IconButton;
